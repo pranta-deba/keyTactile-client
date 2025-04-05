375e10fb3c7179330a9a2ab21ef59f2ff164e6f7
Pranto Deb <rajprit0m@outlook.com>
2025-04-05 21:28:10 +0600

    [Theme] : system, light and dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { useEffect } from "react";
+import ThemeToggle from "./components/ThemeToggle/ThemeToggle";
 import { Button } from "./components/ui/button";
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
+  const currentTheme = useAppSelector((state) => state.theme.theme);
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.classList.remove("light", "dark");
+
+    if (currentTheme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
+      root.classList.add(systemTheme);
+    } else {
+      root.classList.add(currentTheme);
+    }
+  }, [currentTheme]);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="flex flex-col items-center justify-center min-h-svh">
+      <div className="">
         <Button>Click me</Button>
+      </div>
+      <div>
+        <ThemeToggle />
       </div>
     </>
   );
