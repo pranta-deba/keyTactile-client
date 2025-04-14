40ef9fed89bc3e8d96e867b0f3130c15631a9207
Pranto Deb <rajprit0m@outlook.com>
2025-04-14 22:15:20 +0600

    [Redux] : [Base Api] : invalid token to logout

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,11 +1,43 @@
 // baseApi.ts
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { logout } from "../features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("Authorization", token);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithExpiredToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  const result = await baseQuery(args, api, extraOptions);
+  // invalid token
+  if (result?.error?.status === 401) {
+    api.dispatch(logout());
+  }
+  return result;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
-    credentials: "include",
-  }),
+  baseQuery: baseQueryWithExpiredToken,
   endpoints: () => ({}),
 });
