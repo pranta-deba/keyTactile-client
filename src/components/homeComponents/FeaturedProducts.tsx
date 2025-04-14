cf624bb18b99d0ba54ab38e038aeb0ae5d977eae
Pranto Deb <rajprit0m@outlook.com>
2025-04-14 22:56:02 +0600

    [Redux] : [Products] : get all products

--- src/components/homeComponents/FeaturedProducts.tsx
+++ src/components/homeComponents/FeaturedProducts.tsx
@@ -1,48 +1,33 @@
-import { featuredProducts } from "@/constants/home.constants";
-import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
-import { Star } from "lucide-react";
+import { Loader } from "lucide-react";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
+import { useGetAllProductsQuery } from "@/redux/features/products/productsApi";
+import AppProductCard from "../productComponents/AppProductCard";
+import { TProduct } from "@/types/products.types";
 
 const FeaturedProducts = () => {
+  const { data: products, isLoading } = useGetAllProductsQuery(undefined);
+
+  console.log(products, isLoading);
+
+  if (isLoading) {
+    return (
+      <div className="h-20 w-full flex justify-center items-center">
+        <p>
+          <Loader className="animate-spin" />
+        </p>
+      </div>
+    );
+  }
+
   return (
     <section id="featured" className="container mx-auto px-4">
       <h2 className="text-3xl font-bold mb-8 text-center md:text-start">
         Featured Products
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6">
-        {featuredProducts.map((product) => (
-          <Card key={product._id} >
-            <CardHeader>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="w-full h-48 object-cover rounded-t-lg"
-              />
-            </CardHeader>
-            <CardContent className="flex flex-col gap-2">
-              <h3 className="font-semibold text-lg">{product.name}</h3>
-              <p className="text-muted-foreground">{product.brand}</p>
-              <div className="flex items-center gap-2">
-                {Array.from({ length: 5 }).map((_, i) => (
-                  <Star
-                    key={i}
-                    className={`w-4 h-4 ${
-                      i < product.rating
-                        ? "text-yellow-400 fill-yellow-400"
-                        : "text-gray-300"
-                    }`}
-                  />
-                ))}
-              </div>
-              <p className="font-bold">${product.price}</p>
-            </CardContent>
-            <CardFooter>
-              <Button className="w-full" asChild>
-                <Link to={`/products/${product._id}`}>View Details</Link>
-              </Button>
-            </CardFooter>
-          </Card>
+        {products?.data?.map((product: TProduct) => (
+          <AppProductCard key={product._id} {...product} />
         ))}
       </div>
       <div className="mt-8 text-center">
---
+++ src/components/productComponents/AppProductCard.tsx
@@ -0,0 +1,48 @@
+import { TProduct } from "@/types/products.types";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { Star } from "lucide-react";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
+const AppProductCard = ({
+  _id,
+  title,
+  brand,
+  price,
+  rating,
+  images,
+}: TProduct) => {
+  return (
+    <Card key={_id}>
+      <CardHeader>
+        <img
+          src={images?.length > 0 ? images[0] : ""}
+          alt={title}
+          className="w-full h-48 object-cover rounded-t-lg"
+        />
+      </CardHeader>
+      <CardContent className="flex flex-col gap-2">
+        <h3 className="font-semibold text-lg">{title}</h3>
+        <p className="text-muted-foreground">{brand}</p>
+        <div className="flex items-center gap-2">
+          {Array.from({ length: 5 }).map((_, i) => (
+            <Star
+              key={i}
+              className={`w-4 h-4 ${
+                i < rating ? "text-yellow-400 fill-yellow-400" : "text-gray-300"
+              }`}
+            />
+          ))}
+        </div>
+        <p className="font-bold">${price}</p>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full" asChild>
+          <Link to={`/products/${_id}`}>View Details</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default AppProductCard;
