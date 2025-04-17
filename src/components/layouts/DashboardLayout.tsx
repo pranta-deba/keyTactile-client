3cadd76847b12ef27146f2a1b79bdbb4552059ad
Pranto Deb <rajprit0m@outlook.com>
2025-04-18 00:02:42 +0600

    [Layout] : [Dashboard] : [Sidebar]

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -1,8 +1,47 @@
+import { items } from "@/constants/sidebar.items";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "../ui/sidebar";
+
 const DashboardLayout = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <SidebarProvider>
+       <SidebarTrigger />
+      <div>
+        <div>
+          <Sidebar>
+            <SidebarContent>
+              <SidebarGroup>
+                <SidebarGroupLabel>Application</SidebarGroupLabel>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    {items.map((item) => (
+                      <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton asChild>
+                          <a href={item.url}>
+                            <item.icon />
+                            <span>{item.title}</span>
+                          </a>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    ))}
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </SidebarGroup>
+            </SidebarContent>
+          </Sidebar>
+        </div>
+      </div>
+    </SidebarProvider>
   );
 };
 
