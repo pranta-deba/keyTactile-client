afb11193c91ddec20ce8af2a05e28ad796c6a0fc
Pranto Deb <rajprit0m@outlook.com>
2025-04-18 22:02:18 +0600

    [Page] : [Home] : [Features bands shown by marquee] 2

--- src/components/homeComponents/FeaturedBrands.tsx
+++ src/components/homeComponents/FeaturedBrands.tsx
@@ -1,18 +1,24 @@
 import { useGetAllBrandsQuery } from "@/redux/features/brands/brandsApi";
+import { Loader } from "lucide-react";
 import Marquee from "react-fast-marquee";
 
 const FeaturedBrands = () => {
-  const { data } = useGetAllBrandsQuery({});
+  const { data, isLoading } = useGetAllBrandsQuery({});
   const allBrands = data?.data || [];
   return (
     <section className="container mx-auto px-4">
       <h2 className="text-3xl font-bold mb-8 text-center md:text-start">
         Featured Brands
       </h2>
-      <Marquee
-      pauseOnHover={true}
-      >
-        {allBrands?.slice(0,10).map((brand) => (
+      {isLoading && (
+        <div className="h-20 w-full flex justify-center items-center">
+          <p>
+            <Loader className="animate-spin" />
+          </p>
+        </div>
+      )}
+      <Marquee pauseOnHover={true}>
+        {allBrands?.slice(0, 10).map((brand) => (
           <figure key={brand._id} className="mx-10">
             <img
               src={brand.image}
