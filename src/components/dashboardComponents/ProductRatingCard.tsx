5c91d9b0066a338c4b503f7f86052a937e7bc31e
Pranto Deb <rajprit0m@outlook.com>
2025-04-21 23:36:45 +0600

    [Dashboard] : product component 5

---
+++ src/components/dashboardComponents/ProductRatingCard.tsx
@@ -0,0 +1,28 @@
+const ProductRatingCard = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ipsa? Vero quaerat voluptate ea nostrum, laborum odit aspernatur fugiat aliquid quasi illum. Ab pariatur ducimus minus quidem, reprehenderit quis culpa.
+            Ad ducimus beatae repudiandae ratione in est optio alias, pariatur fugit laborum, provident, quasi esse. Facere, labore voluptate possimus qui animi accusamus modi ex mollitia. Neque enim eveniet animi quibusdam.
+            Libero sunt quis quisquam deleniti at, nesciunt mollitia ipsam a magni dolorum illum in ex esse autem amet quasi, est quam quibusdam repudiandae natus. Assumenda porro enim dignissimos maxime praesentium!
+            Quis aliquid quod maiores labore dignissimos voluptas qui dolor odit, eaque deleniti debitis, nemo ipsum quas autem magni repellat! Sunt quis, cupiditate provident at odit sint inventore natus nesciunt exercitationem!
+            Id ex recusandae numquam ea vitae saepe, illum vel placeat? Tempore et quos veniam, sapiente natus cupiditate illo earum doloribus deserunt temporibus mollitia eligendi, asperiores nihil quasi? Quasi, mollitia minima.
+            Sapiente, animi et aspernatur quae quam temporibus minima accusamus, quis eius ducimus ullam corrupti nostrum incidunt! Voluptatum, aliquam! Quo corrupti blanditiis iste eos harum ipsa quas et excepturi! Deleniti, repellendus.
+            Vitae commodi vero iusto saepe nesciunt quaerat error incidunt quam deleniti hic, officiis odio quis aliquid fuga alias nemo voluptates. Vero earum dolores excepturi quam eligendi provident beatae dicta accusamus.
+            Eaque obcaecati dolore itaque magnam eius? Dolore praesentium sint ea et quibusdam velit aut voluptates voluptatibus delectus minima, est, explicabo quaerat consequuntur cum atque illum dolorem. Excepturi quisquam dignissimos omnis.
+            Eum totam minus modi beatae fugiat esse sunt assumenda quisquam iusto placeat repellendus consectetur hic, deserunt, nam tenetur expedita magni. Voluptatem iste, blanditiis voluptates iusto vero eos labore aliquam omnis.
+            Eos, inventore dolore. Temporibus maiores dolorum tempore perferendis quo eaque consectetur. Eaque temporibus quas quisquam maiores voluptas dicta debitis voluptatum totam, illo odit ipsa architecto saepe necessitatibus cum vitae doloribus.
+            Excepturi vel doloribus delectus. Dignissimos ducimus repellendus vel fugit ab ex. Corrupti praesentium dicta eveniet repellendus cumque soluta modi esse! Nemo, corporis! Maxime veritatis vitae nostrum placeat ut consequatur aspernatur?
+            Necessitatibus aut accusantium facilis est impedit sapiente minus esse qui cum earum enim sit nemo, praesentium ex vero, illum corrupti placeat voluptates. Alias in, culpa quod et maiores vero consequatur.
+            Velit dicta incidunt in ab aperiam vero, quam libero est numquam beatae corrupti natus aspernatur quidem asperiores a accusamus modi ipsum assumenda ipsam iusto. Saepe ducimus architecto id ad quae?
+            Doloremque labore id sunt nemo eius molestias ex iure cum illo quos ipsa dolore repellat error optio sed ab unde modi quas vero nostrum, nam perspiciatis exercitationem non! Velit, quis.
+            Quos quidem fugiat enim, delectus eum numquam recusandae reiciendis, quasi incidunt vitae asperiores esse porro quis. Eaque laboriosam vel provident dignissimos eveniet asperiores modi consectetur, ut voluptatibus, totam, doloribus eos?
+            Sit, expedita. Eius adipisci iusto hic? Exercitationem ad voluptatibus cupiditate fuga! Sed, explicabo autem. Voluptate quas quidem porro corrupti aliquid debitis eos, nemo in, eligendi dolor saepe, rerum deleniti ad?
+            Temporibus provident nesciunt, beatae sequi fugiat officiis, facere, quia quidem vel doloremque dignissimos quam rem dolor perferendis vitae culpa itaque ipsam? Inventore nobis incidunt quod molestias excepturi laboriosam tenetur perferendis.
+            Expedita, eius quisquam libero recusandae quae, eaque, voluptate odio iure a aspernatur sed est officia cumque harum impedit natus aliquid maiores nihil voluptates atque repellendus temporibus fugiat fugit! Sapiente, unde?
+            Commodi vero officia nemo non quaerat! Quis perferendis cum et sunt nulla ea amet veritatis temporibus. Explicabo sunt praesentium ratione? Architecto reprehenderit sapiente nulla voluptatibus ipsum eveniet ipsam, perferendis dolore.
+            Illum voluptates laboriosam tempora nihil magnam, expedita veniam cupiditate nulla blanditiis consequuntur, error fugiat optio animi, omnis quibusdam aliquam velit ut incidunt minima consectetur ipsum soluta ex eum perspiciatis? Qui?</h1>
+        </div>
+    );
+};
+
+export default ProductRatingCard;
