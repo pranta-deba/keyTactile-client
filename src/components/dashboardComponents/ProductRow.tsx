a76b111b95fa4bbc5cb6f31af1694655c49963bc
Pranto Deb <rajprit0m@outlook.com>
2025-04-21 23:34:40 +0600

    [Dashboard] : product component

---
+++ src/components/dashboardComponents/ProductRow.tsx
@@ -0,0 +1,18 @@
+const ProductRow = () => {
+    return (
+        <div>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor, perspiciatis quo tempora molestiae officia nisi omnis eum id, accusamus eveniet est ipsa reiciendis eligendi nulla quisquam natus aspernatur a.
+           Distinctio praesentium aperiam ea mollitia modi quod quam nostrum nemo! Totam aperiam optio numquam esse. Qui, eligendi rem quos assumenda sunt fugiat. Architecto repellendus iusto quaerat omnis iste deleniti dicta!
+           Quaerat blanditiis minima sint, esse vitae necessitatibus molestiae, cupiditate ratione mollitia ipsum voluptatum sit nemo, possimus earum dolorem soluta illo enim maxime accusantium a tempora voluptas? Aspernatur iste tempora itaque.
+           Possimus blanditiis molestiae quod assumenda accusamus vitae eius animi quos aliquam aliquid. Corporis nihil aspernatur reprehenderit omnis at cum beatae aliquam animi error. Pariatur veniam, eveniet quod consequuntur consectetur doloremque.
+           Aperiam rerum distinctio sit quasi doloremque, beatae, minima error perferendis minus eaque officia provident, laboriosam pariatur repellendus ea architecto quis. Modi debitis a incidunt, libero sequi quo suscipit cum neque.
+           Repellat cupiditate, saepe laborum impedit ex quis architecto minus consectetur. Doloremque, doloribus consequatur suscipit obcaecati animi facilis. Illo, quas incidunt fugit tempore ut blanditiis molestias a in obcaecati ex est.
+           Veniam, eum rem sapiente consectetur aliquam blanditiis, labore sint dolore explicabo id culpa. Optio eveniet vitae magni accusamus magnam cumque voluptates, quae, nemo minus, repellat voluptas fugiat asperiores dolore inventore?
+           Sequi eligendi illo excepturi! Ab fuga aliquid voluptatum nemo nihil ipsam minus perferendis, similique ratione sint labore explicabo aliquam ullam distinctio animi autem quasi iure magni nesciunt. Debitis, unde velit?
+           Voluptatum distinctio corporis vel optio pariatur ducimus suscipit laudantium corrupti unde, voluptatibus, libero, soluta at alias aliquam nesciunt veniam iste ipsa deserunt non facere amet sit. Inventore quibusdam quis error.
+           Beatae libero voluptates architecto iusto quidem ullam expedita repellendus nobis maiores quibusdam, eligendi, odit excepturi error vitae velit? Odio soluta perspiciatis sed nulla dolor recusandae reprehenderit exercitationem omnis minus. Perspiciatis.
+        </div>
+    );
+};
+
+export default ProductRow;
