26d542d3d9536c694008f2af335b7a75649fd57c
Pranto Deb <rajprit0m@outlook.com>
2025-04-21 23:35:23 +0600

    [Dashboard] : product component 2

---
+++ src/components/dashboardComponents/ProductCol.tsx
@@ -0,0 +1,13 @@
+const ProductCol = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, error. Nesciunt perspiciatis, tenetur aspernatur quasi consequatur, cumque quam impedit quidem debitis suscipit commodi esse vero culpa dolorum accusamus cupiditate quod?
+            At inventore, facilis laudantium sed corporis itaque ipsum odit! Ex magni eligendi, itaque quo, vero consectetur aperiam maxime aliquid rem eius omnis repudiandae magnam autem ea laboriosam nobis architecto quaerat?
+            Beatae, veritatis natus. Hic exercitationem nisi quae pariatur voluptate saepe nihil culpa optio, accusantium suscipit mollitia, reiciendis soluta! Corrupti ab totam corporis incidunt exercitationem officiis, ratione quos commodi temporibus pariatur!
+            Numquam sed recusandae quo tenetur vitae aliquid libero placeat amet nostrum iusto dolor quae excepturi atque at eveniet quidem eius cum explicabo, laboriosam praesentium, ab quod? Alias inventore nostrum odio!
+            Omnis eos quod aperiam iusto odio nesciunt fugit ex voluptates aliquam veniam cupiditate expedita, impedit natus a! Excepturi, expedita incidunt molestias, dicta animi molestiae temporibus enim provident, modi odit deserunt.</h1>
+        </div>
+    );
+};
+
+export default ProductCol;
