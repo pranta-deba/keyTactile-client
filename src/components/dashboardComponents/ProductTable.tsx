f74fc3c788ebbac3ab96bdd6979d978f981b6e42
Pranto Deb <rajprit0m@outlook.com>
2025-04-21 23:35:49 +0600

    [Dashboard] : product component 3

---
+++ src/components/dashboardComponents/ProductTable.tsx
@@ -0,0 +1,19 @@
+const ProductTable = () => {
+    return (
+        <div>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quisquam. Quo eveniet veniam alias nisi doloribus, inventore a natus, maiores repudiandae quisquam mollitia ex, in officia assumenda libero maxime voluptates.
+           Quam, unde fugiat distinctio doloremque itaque modi sit! Eaque voluptatem dolor numquam fugit possimus, porro ex at! Debitis sit eius voluptatem, quis ducimus reprehenderit qui facilis, perferendis corrupti accusamus labore?
+           Natus distinctio totam iusto, sequi molestiae quidem qui error beatae. Ipsum similique, accusantium molestias vitae odit, quam amet ipsa hic quod voluptas rerum minus cupiditate accusamus ratione. Consectetur, reprehenderit quod!
+           Suscipit sit hic, quasi quos esse vitae. Quas repellendus quod est. Quam, placeat iste. Iusto voluptate nulla architecto eos inventore excepturi magnam et, quos rem eaque? Quo soluta est laborum!
+           Quo dolore aperiam optio labore eligendi explicabo, maiores animi aliquid autem! Eum dignissimos sequi aliquam dolorum recusandae, iure iusto. Veniam debitis eius accusamus quis recusandae quibusdam modi vitae enim voluptates.
+           Alias, cupiditate corporis! Rerum rem quam, voluptas repudiandae aliquid animi, quisquam consectetur id pariatur explicabo, corrupti maiores ea repellendus impedit perferendis incidunt unde necessitatibus deserunt iste quasi placeat adipisci! Molestias!
+           Optio praesentium ad repudiandae, veniam eveniet dicta qui odit nihil voluptates id aliquid, soluta labore maxime at? Maiores consequatur quia perspiciatis labore tenetur cumque magnam, vel, officiis enim quod tempora.
+           A nesciunt quia ipsum voluptatem nemo voluptatum, quos vitae sunt explicabo ex numquam eveniet voluptate rerum sint ratione iste omnis? Quisquam, voluptatem iste? Exercitationem blanditiis perspiciatis, aspernatur error consequuntur ipsum!
+           Asperiores, cum. Nulla animi asperiores qui quod illum ipsam eius fugit, dicta eligendi tenetur, temporibus quidem modi neque ex sequi porro dignissimos amet iure voluptatibus placeat odit eaque libero aperiam.
+           Asperiores, quod dolore vitae eum facere repellat cumque exercitationem consequuntur, quos earum, doloribus laborum reprehenderit quidem voluptatum ipsum velit aspernatur expedita. Repellat alias exercitationem accusantium, quidem id corporis nemo earum?
+           Debitis, quae deleniti pariatur ad aliquam excepturi quisquam odio, beatae eum culpa inventore. Maxime hic nobis sapiente magnam, aut excepturi? Quaerat nostrum magni facilis voluptates provident, quas a perferendis rerum?
+        </div>
+    );
+};
+
+export default ProductTable;
